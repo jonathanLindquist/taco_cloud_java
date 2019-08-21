3e53437a1d3fe669b5c2f0d2079422b20965d924
Jonathan Lindquist <jonathan.lindquist17@gmail.com>
2019-08-20 23:04:51 -0500

    starting to use h2 internal db

--- src/main/java/tacos/domain/Order.java
+++ src/main/java/tacos/domain/Order.java
@@ -6,9 +6,12 @@ import org.hibernate.validator.constraints.CreditCardNumber;
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
+import java.util.Date;
 
 @Data
 public class Order {
+	private Long id;
+	private Date createDate;
 
 	@NotBlank(message="Name is required")
 	private String name;
--- src/main/java/tacos/domain/Taco.java
+++ src/main/java/tacos/domain/Taco.java
@@ -5,11 +5,14 @@ import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.Date;
 import java.util.List;
 
 @Data
 @NoArgsConstructor
 public class Taco {
+	private Long id;
+	private Date createDate;
 
 	@NotNull
 	@Size(min=5, message="Name must be at least 5 characters long")
