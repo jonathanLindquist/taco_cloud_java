b130eea900c98fe3b5443b1f0f5272ea563a0315
Jonathan Lindquist <jonathan.lindquist17@gmail.com>
2019-08-20 19:14:31 -0500

    completing validation in the form data

--- src/main/java/tacos/controllers/design/DesignTacoController.java
+++ src/main/java/tacos/controllers/design/DesignTacoController.java
@@ -3,12 +3,14 @@ package tacos.controllers.design;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import tacos.domain.Ingredient;
 import tacos.domain.Taco;
 
+import javax.validation.Valid;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -44,7 +46,10 @@ public class DesignTacoController {
 	}
 
 	@PostMapping
-	public String processDesign(Taco taco) {
+	public String processDesign(@Valid Taco taco, Errors errors) {
+		if (errors.hasErrors()) {
+			return "design/design";
+		}
 		//save taco design, will do in chapter 3
 		log.info("Processing Design " + taco);
 
