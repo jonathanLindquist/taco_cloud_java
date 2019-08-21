5e075cfa66b2f3aa008dd432dadee1f791971a99
Jonathan Lindquist <jonathan.lindquist17@gmail.com>
2019-08-20 19:00:33 -0500

    adding validation to domain objects

--- src/main/java/tacos/domain/Taco.java
+++ src/main/java/tacos/domain/Taco.java
@@ -3,12 +3,18 @@ package tacos.domain;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Data
 @NoArgsConstructor
 public class Taco {
+
+	@NotNull
+	@Size(min=5, message="Name must be at least 5 characters long")
 	private String name;
 
+	@Size(min=1, message="You must choose at least 1 ingredient")
 	private List<String> ingredients;
 }
