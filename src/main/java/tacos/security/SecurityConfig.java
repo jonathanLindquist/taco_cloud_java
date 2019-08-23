9adf5de9399d264da8ca608a4d04d563df90e3d8
Jonathan Lindquist <n105556@icf.com>
2019-08-23 14:34:52 -0500

    configuring security for LDAP, didnt work well

--- src/main/java/tacos/security/SecurityConfig.java
+++ src/main/java/tacos/security/SecurityConfig.java
@@ -8,12 +8,45 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+//	@Autowired
+//	DataSource dataSource;
+
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
+//		ldap authentication configuration
 
-//	in-memory example of user configuration
+//		auth.ldapAuthentication()
+//				.userSearchBase("ou=people")
+//				.userSearchFilter("(uid={0})")
+//				.groupSearchBase("ou=groups")
+//				.groupSearchFilter("member={0}")
+//				.passwordCompare() //compare passwords on the LDAP server directly
+//				.passwordEncoder(new BCryptPasswordEncoder()) //encrypt for API Call, password on LDAP must also be encoded in similar format
+//				.passwordAttribute("passcode"); //name of password field on LDAP server, defaults to checking 'userPassword'
+//
+//		auth.ldapAuthentication()
+//				.contextSource() //use contextSource if LDAP server is not embedded, set URL to destination
+//				.root("dc=tacocloud,dc=com") // use root for embedded LDAP server
+//				.ldif("classpath:users.ldif"); // specify where ldap server should load user data
+//				.url("ldap://tacocloud.com:389/dc=tacocloud,dc=com");
 
+
+//		----------jdbc authentication defining specific queries and password enoding-------------
+//
+//		auth.jdbcAuthentication()
+//				.dataSource(dataSource)
+//				.usersByUsernameQuery(
+//						"select username, password, enabled from Users " +
+//								"where username=?")
+//				.authoritiesByUsernameQuery(
+//						"select username, authority from UserAuthorities " +
+//								"where username=?")
+//				.passwordEncoder(new BCryptPasswordEncoder());
+
+//		----------in-memory example of user configuration-----------
+//
 //		auth.inMemoryAuthentication()
 //				.withUser("buzz")
 //				.password("infinity")
--- src/test/java/com/tacocloud/TacoCloudApplicationTests.java
+++ src/test/java/com/tacocloud/TacoCloudApplicationTests.java
@@ -1,4 +1,4 @@
-package springinaction.tacocloud;
+package com.tacocloud;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
