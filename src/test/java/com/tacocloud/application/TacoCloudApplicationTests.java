31d184de6dbdd71c31656b3ce1cc0ac12f7a9529
Jonathan Lindquist <n105556@icf.com>
2019-08-27 15:50:04 -0500

    fixing test

--- src/test/java/com/tacocloud/application/TacoCloudApplicationTests.java
+++ src/test/java/com/tacocloud/application/TacoCloudApplicationTests.java
@@ -1,8 +1,12 @@
 package com.tacocloud.application;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.containsString;
@@ -11,17 +15,20 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
-@WebMvcTest
+@WebMvcTest // Spring-Boot test auto-configure
+//@RunWith(SpringRunner.class) //using JUnit 4
+//@SpringBootTest
+//@AutoConfigureMockMvc
 class TacoCloudApplicationTests {
 
 	@Autowired
 	private MockMvc mockMvc;
 
 	@Test
-	public void testHomePage() throws Exception {
+	public void testLoginPage() throws Exception {
 		mockMvc.perform(get("/"))
 				.andExpect(status().isOk())
-				.andExpect(view().name("Home"))
+				.andExpect(view().name("home"))
 				.andExpect(content().string(containsString("Welcome to...")));
 	}
 
