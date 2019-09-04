0c554579fce0b48b98b020415c7bf3aeb1e49953
Jonathan Lindquist <n105556@icf.com>
2019-09-04 09:22:42 -0500

    comments and moving annotations around

--- src/main/java/com/tacocloud/TacoCloudApplication.java
+++ src/main/java/com/tacocloud/TacoCloudApplication.java
@@ -3,10 +3,11 @@ package com.tacocloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+// annotations below are necessary if the base packages aren't in the @SpringBootApplication's (main method) package
 //@SpringBootApplication(scanBasePackages = "com.tacocloud")
 //@EnableJpaRepositories(basePackages = "com.tacocloud.application.tacos.data")
 //@EntityScan(basePackages = "com.tacocloud.application.tacos.domain")
+@SpringBootApplication
 public class TacoCloudApplication {
 
 	public static void main(String[] args) {
