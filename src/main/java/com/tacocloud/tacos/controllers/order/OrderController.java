8e2b7c178bf96cbfdeb083ca2220b088b4c0a662
Jonathan Lindquist <n105556@icf.com>
2019-09-24 10:33:14 -0500

    refactor controllers for some REST work

--- src/main/java/com/tacocloud/tacos/controllers/order/OrderController.java
+++ src/main/java/com/tacocloud/tacos/controllers/order/OrderController.java
@@ -5,19 +5,27 @@ import com.tacocloud.tacos.domain.Order;
 import com.tacocloud.tacos.domain.User;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 
 import javax.validation.Valid;
+import java.util.Optional;
 
 @Slf4j
 @Controller
@@ -62,5 +70,54 @@ public class OrderController {
 		sessionStatus.setComplete();
 
 		return "redirect:/";
+	}
+
+	@PatchMapping(path="/{orderId}", consumes="application/json")
+	public Order patchOrder(@PathVariable("orderId") Long orderId,
+			@RequestBody Order patch) {
+		Optional<Order> optionalOrder = orderRepository.findById(orderId);
+		if (optionalOrder.isPresent()) {
+			Order order = optionalOrder.get();
+			if (patch.getName() != null) {
+				order.setName(patch.getName());
+			}
+			if (patch.getStreet() != null) {
+				order.setStreet(patch.getStreet());
+			}
+			if (patch.getCity() != null) {
+				order.setCity(patch.getCity());
+			}
+			if (patch.getState() != null) {
+				order.setState(patch.getState());
+			}
+			if (patch.getZip() != null) {
+				order.setZip(patch.getZip());
+			}
+			if (patch.getCcNumber() != null) {
+				order.setCcNumber(patch.getCcNumber());
+			}
+			if (patch.getCcExpiration() != null) {
+				order.setCcExpiration(patch.getCcExpiration());
+			}
+			if (patch.getCcCVV() != null) {
+				order.setCcCVV(patch.getCcCVV());
+			}
+
+			return orderRepository.save(order);
+		}
+
+		return null;
+	}
+
+	@DeleteMapping("/{orderId}")
+	@ResponseStatus(code= HttpStatus.NO_CONTENT)
+	public void deleteOrder(@PathVariable("orderId") Long orderId) {
+		try {
+			orderRepository.deleteById(orderId);
+		} catch (EmptyResultDataAccessException e) {
+			log.info(e.getMessage());
+		} catch (Exception e) {
+			log.error(e.getMessage());
+		}
 	}
 }
--- src/main/java/com/tacocloud/tacos/rest/controllers/RestDesignTacoController.java
+++ src/main/java/com/tacocloud/tacos/rest/controllers/RestDesignTacoController.java
@@ -8,7 +8,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
@@ -39,5 +42,11 @@ public class RestDesignTacoController {
 		}
 
 		return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+	}
+
+	@PostMapping(consumes = "application/json")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Taco postTaco(@RequestBody Taco taco) {
+		return tacoRepository.save(taco);
 	}
 }
